da05a7c4300a666c7493599a9f9c7eb477dc0f82
google123abhi <googelabhi1arcade@gmail.com>
2025-08-03 21:04:30 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: false
-  }
   optimizeDeps: {
-    exclude: ['lucide-react'],
+  include: ['lodash', 'axios'],
   },
 });
